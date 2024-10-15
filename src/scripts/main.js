4e80f4f1a51aa958260358a65ffebfcef9da89a8
dima <shymdima74@gmail.com>
2024-10-15 11:15:52 +0300

    addTaskSolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,22 +5,22 @@ wall.addEventListener('click', (e) => {
   const spider = document.querySelector('.spider');
   const spiderRect = spider.getBoundingClientRect();
   const wallRect = wall.getBoundingClientRect();
-   const clickX = e.clientX - wallRect.left;
-   const clickY = e.clientY - wallRect.top;
+  const clickX = e.clientX - wallRect.left;
+  const clickY = e.clientY - wallRect.top;
  
-   let topPosition = clickY - (spiderRect.height / 2);
-   let leftPosition = clickX - (spiderRect.width / 2);
+  let topPosition = clickY - (spiderRect.height / 2);
+  let leftPosition = clickX - (spiderRect.width / 2);
 
   if (topPosition < 0) {
     topPosition = 0;
-  } else if (topPosition > wallRect.height) {
-    topPosition = wallRect.height;
+  } else if (topPosition + spiderRect.height > wallRect.height) {
+    topPosition = wallRect.height - spiderRect.height;
   }
 
-  if (leftPosition < 0) {
+  if (leftPosition  < 0) {
     leftPosition = 0;
-  } else if (leftPosition > wallRect.width) {
-    leftPosition = wallRect.width;
+  } else if (leftPosition + spiderRect.width > wallRect.width) {
+    leftPosition = wallRect.width - spiderRect.width;
   }
 
   spider.style.top = `${topPosition}px`;
